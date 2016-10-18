2335b36ffd718b187002935d1461d4abe46b3145
Ankur <ankurks@thoughtworks.com>
2016-10-18 23:52:59 +0530

    Adding getters and setters for direction and coordinates

--- src/rover/Rover.java
+++ src/rover/Rover.java
@@ -23,30 +23,46 @@ public class Rover {
         if(command == 'M') move();
     }
 
+    public void setCordinate(Cordinate cordinate){
+        this.cordinate = cordinate;
+    };
+
+    public void setDirection(Direction direction){
+        this.direction = direction;
+    }
+
+    public Cordinate getCordinate(){
+        return this.cordinate;
+    }
+
+    public Direction getDirection(){
+        return this.direction;
+    }
+
     private void move() {
         switch (this.direction){
-            case E: {this.cordinate.x = this.cordinate.x + 1; break;}
-            case W: {this.cordinate.x = this.cordinate.x - 1; break;}
-            case N: {this.cordinate.y = this.cordinate.y + 1; break;}
-            case S: {this.cordinate.y = this.cordinate.y - 1; break;}
+            case E: {this.setCordinate(new Cordinate(this.cordinate.x + 1, this.cordinate.y)); break;}
+            case W: {this.setCordinate(new Cordinate(this.cordinate.x - 1, this.cordinate.y )); break;}
+            case N: {this.setCordinate(new Cordinate(this.cordinate.x, this.cordinate.y + 1)); break;}
+            case S: {this.setCordinate(new Cordinate(this.cordinate.x, this.cordinate.y -1)); break;}
         }
     }
 
     private void turnRight() {
         switch (this.direction){
-            case E: {this.direction = Direction.S; break;}
-            case W: {this.direction = Direction.N; break;}
-            case N: {this.direction = Direction.E; break;}
-            case S: {this.direction = Direction.W; break;}
+            case E: {this.setDirection(Direction.S); break;}
+            case W: {this.setDirection(Direction.N); break;}
+            case N: {this.setDirection(Direction.E); break;}
+            case S: {this.setDirection(Direction.W); break;}
         }
     }
 
     private void turnLeft() {
         switch (this.direction){
-            case E: {this.direction = Direction.N; break;}
-            case W: {this.direction = Direction.S; break;}
-            case N: {this.direction = Direction.W; break;}
-            case S: {this.direction = Direction.E; break;}
+            case E: {this.setDirection(Direction.N); break;}
+            case W: {this.setDirection(Direction.S); break;}
+            case N: {this.setDirection(Direction.W); break;}
+            case S: {this.setDirection(Direction.E); break;}
         }
     }
 }
--- test/rover/RoverTest.java
+++ test/rover/RoverTest.java
@@ -7,6 +7,20 @@ import static org.junit.Assert.*;
 
 public class RoverTest {
     @Test
+    public void shouldSetNewCordinate() throws Exception {
+        Rover rover = new Rover(new Cordinate(2,2), Direction.E);
+        rover.setCordinate(new Cordinate(3,3));
+        assertTrue(rover.getCordinate().equals(new Cordinate(3, 3)));
+    }
+
+    @Test
+    public void shouldSetNewDirection() throws Exception {
+        Rover rover = new Rover(new Cordinate(2,2), Direction.E);
+        rover.setDirection(Direction.N);
+        assertThat(rover.getDirection(), is(Direction.N));
+    }
+
+    @Test
     public void shouldGiveRightResultAfterProcessingCommandString() throws Exception {
         Cordinate cordinate = new Cordinate(3,3);
         Rover rover = new Rover(cordinate, Direction.E);
